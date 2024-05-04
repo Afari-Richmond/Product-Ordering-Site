21a2ee1f02d13a4fd4e5cf94aae617a01a0b2fab
Afari Richmonnd Addo <afaririchmond1000@gmail.com>
2024-05-04 20:03:24 +0000

    added functionality to the  home icon

--- public/script.js
+++ public/script.js
@@ -1,2 +1,35 @@
+//functioality to direct to hompage when logo is clicked
+document.getElementById('cart-logo').addEventListener('click', ()=>{
+    window.location.href = '/public/index.html'
+})
+
+//drop-down menu for the categories link
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 document.getElementById('currentYear').textContent = new Date().getFullYear()
